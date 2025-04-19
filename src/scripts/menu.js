1f230e230a9ca3d802ae738534966be1073c23bb
xfokiux xfokiux <xfokiuxxfokiux@Diegos-MacBook-Air.local>
2025-04-19 16:38:08 -0500

    adding Hamburger menu  scripts

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-  console.log('it is clicked');
-  // document.querySelector('.nav-links').classList.toggle('expanded');
+   document.querySelector('.nav-links').classList.toggle('expanded');
 });
